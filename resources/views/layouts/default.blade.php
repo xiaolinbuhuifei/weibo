32f515455ab3b99ef618538518b2cf8fd2e3d959
Linzeqin <1093684305@qq.com>
2020-09-13 14:07:54 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta chaset="utf-8">
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
 
